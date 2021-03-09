66af950fa261f38d3f842ed9a30f9908a465f004
Joshua Del Rosario <joshuadaviddelrosario@gmail.com>
2021-03-09 21:35:54 +0800

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Joshua Del Rosario on 3/9/21.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
