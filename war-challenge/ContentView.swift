0b5add6cb7d5199d1971e5072af631e8e0b62a86
Joshua Del Rosario <joshuadaviddelrosario@gmail.com>
2021-03-20 21:20:08 +0800

    initial commit

--- war-challenge/ContentView.swift
+++ war-challenge/ContentView.swift
@@ -8,9 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "card2"
+    @State private var cpuCard = "card5"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            
+            Image("background")
+                .ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                        .shadow(color: .black, radius:10, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 10)
+                    Spacer()
+                    Image(cpuCard).shadow(color: .black, radius:10, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 10)
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    
+                    // Generate random
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    // Update cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    // Update scores
+                    if playerRand > cpuRand{
+                        playerScore += 1
+                    }else if cpuRand > playerRand{
+                        cpuScore += 1
+                    }
+                    
+                }, label: {
+                    Image("dealbutton")
+                })
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player").font(.headline).padding(.bottom)
+                        Text(String(playerScore)).font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                    }.foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    Spacer()
+                    VStack{
+                        Text("CPU").font(.headline).padding(.bottom)
+                        
+                        Text(String(cpuScore)).font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                    }.foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
     }
 }
 
